385435b0ba42bdb2f3deb3b847998fd92277125b
NicoJstudent <127749524+NicoJstudent@users.noreply.github.com>
2024-03-02 15:49:03 +0200

    Reseptit tallentuu firebaseen

--- barApp/firebase/Config.js
+++ barApp/firebase/Config.js
@@ -1,16 +1,11 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, addDoc, serverTimestamp } from 'firebase/firestore';
-import dotenv from 'dotenv';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+//import dotenv from 'dotenv';
 
-dotenv.config();
+//dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+
 };
 
 
@@ -18,6 +13,12 @@ initializeApp(firebaseConfig);
 
 const firestore = getFirestore();
 
+const RECIPE = 'recipe';
+
 export {
-    firestore
+    firestore,
+    collection,
+    addDoc,
+    RECIPE
 }
+
--- barApp/screens/SubmitDrinkScreen.js
+++ barApp/screens/SubmitDrinkScreen.js
@@ -1,41 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 import styles from '../styles';
+import { firestore, collection, addDoc, RECIPE } from '../firebase/Config';
 
 const SubmitDrinkScreen = ({ navigation }) => {
-    const [name, setName] = useState('');
-    const [newRecipe, setNewRecipe] = useState('');
-  
-    const handleSubmit = () => {
-      console.log('Name:', name);
-      console.log('newRecipe:', newRecipe);
-      setName('');
-      setNewRecipe('');
-    }
+  const [name, setName] = useState('');
+  const [newRecipe, setNewRecipe] = useState('');
+
+  const handleSubmit = async () => {
+    const docRef = await addDoc(collection(firestore, RECIPE), {
+      recipeName: name,
+      recipe: newRecipe
+    }).catch(error => console.log(error))
+    setNewRecipe('')
+    setName('')
+    console.log('Recipe saved.')
+  }
+
   return (
     <View style={styles.container}>
-    <Text style={styles.h1}>Submit new cockai</Text>
-    <View style={styles.formText}>
-      <Text style={styles.inputHeader}>Cocktail name</Text>
-      <TextInput
-        style={styles.formName}
-        value={name}
-        onChangeText={text => setName(text)}
-        placeholder="..."
-      />
+      <Text style={styles.h1}>Submit new cocktail</Text>
+      <View style={styles.formText}>
+        <Text style={styles.inputHeader}>Cocktail name</Text>
+        <TextInput
+          style={styles.formName}
+          value={name}
+          onChangeText={text => setName(text)}
+          placeholder="..."
+        />
+      </View>
+      <View style={styles.formText} >
+        <Text style={styles.inputHeader}>Cocktail recipe</Text>
+        <TextInput
+          style={styles.textbox}
+          value={newRecipe}
+          onChangeText={text => setNewRecipe(text)}
+          placeholder="Recipe:"
+          multiline={true}
+        />
+      </View>
+      <Button style={styles.button} title="Submit" onPress={handleSubmit} />
     </View>
-    <View style={styles.formText} >
-      <Text style={styles.inputHeader}>Cocktail recipe</Text>
-      <TextInput
-        style={styles.textbox}
-        value={newRecipe}
-        onChangeText={text => setNewRecipe(text)}
-        placeholder="Recipe:"
-        multiline={true}
-      />
-    </View>
-    <Button style={styles.button} title="Submit" onPress={handleSubmit} />
-  </View>
   );
 };
 
